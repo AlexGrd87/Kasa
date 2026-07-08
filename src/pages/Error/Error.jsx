56326919719f8654f2859c778678e660b7d5e56d
AlexGrd87 <algaillard@outlook.fr>
2026-07-08 10:43:15 +0200

    Etape 6 : page d'erreur 404
    - Contenu final (code 404, message, lien retour accueil)
    - Toute route inexistante y mene deja via le catch-all du router (etape 3)

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import './Error.scss';
+
 const Error = () => {
-  return <h1>404 - Page non trouvée</h1>;
+  return (
+    <div className="error">
+      <h1 className="error__code">404</h1>
+      <p className="error__message">
+        La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error__link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
