76b01197b5e7d3152478db0baddae93ed548ae7e
AlexGrd87 <algaillard@outlook.fr>
2026-07-08 10:47:35 +0200

    Etape 8 : finalisation de la page logement
    - Titre, localisation, tags, hote (nom + photo), notation en etoiles
    - Description et Equipements via le composant Collapse reutilise
    - Verifie sur plusieurs logements (note differente, image unique)

--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -1,15 +1,62 @@
 import { useParams } from 'react-router-dom';
 import logements from '../../data/logements.json';
 import Slideshow from '../../components/Slideshow/Slideshow';
+import Collapse from '../../components/Collapse/Collapse';
 import './Housing.scss';
 
 const Housing = () => {
   const { id } = useParams();
   const logement = logements.find((item) => item.id === id);
 
+  const { title, location, tags, host, rating, description, equipments, pictures } = logement;
+
   return (
     <div className="housing">
-      <Slideshow pictures={logement.pictures} />
+      <Slideshow pictures={pictures} />
+
+      <div className="housing__info">
+        <div className="housing__details">
+          <h1 className="housing__title">{title}</h1>
+          <p className="housing__location">{location}</p>
+          <ul className="housing__tags">
+            {tags.map((tag) => (
+              <li key={tag} className="housing__tag">
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="housing__host-rating">
+          <div className="housing__host">
+            <p className="housing__host-name">{host.name}</p>
+            <img src={host.picture} alt={host.name} className="housing__host-picture" />
+          </div>
+          <div className="housing__rating">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <span
+                key={star}
+                className={`housing__star ${star <= Number(rating) ? 'housing__star--filled' : ''}`}
+              >
+                ★
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="housing__collapses">
+        <Collapse title="Description">
+          <p>{description}</p>
+        </Collapse>
+        <Collapse title="Équipements">
+          <ul className="housing__equipments">
+            {equipments.map((equipment) => (
+              <li key={equipment}>{equipment}</li>
+            ))}
+          </ul>
+        </Collapse>
+      </div>
     </div>
   );
 };
