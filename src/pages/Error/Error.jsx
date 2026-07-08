36e433df4799e46cd86c439ae20807c529fff963
AlexGrd87 <algaillard@outlook.fr>
2026-07-08 11:15:08 +0200

    Fix page 404 : conformite a la maquette
    - Message prefixe par "Oups! " et colore en corail
    - Lien retour accueil en texte neutre souligne (au lieu du corail)

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
     <div className="error">
       <h1 className="error__code">404</h1>
       <p className="error__message">
-        La page que vous demandez n'existe pas.
+        Oups! La page que vous demandez n'existe pas.
       </p>
       <Link to="/" className="error__link">
         Retourner sur la page d'accueil
